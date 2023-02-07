acf3d12a45962eb20ad35ad827513cdfc72a2566
Beknazar19 <45395471+Beknazar19@users.noreply.github.com>
2023-02-07 22:20:38 +0600

    Add files via upload

--- app_search.js
+++ app_search.js
@@ -1,27 +1,106 @@
+var  tg2 = window.Telegram.WebApp;
+
+tg2.expand(); //расширяем на все окно
+
+tg2.MainButton.text = "Готово"; //изменяем текст кнопки
+tg2.MainButton.setText("Поиск 1"); //изменяем текст кнопки иначе
+tg2.MainButton.textColor = "#e7e8e9"; //изменяем цвет текста кнопки
+tg2.MainButton.color = "#4682B4"; //изменяем цвет бэкграунда кнопки
+tg2.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
+tg2.MainButton.show();
+tg2.MainButton.enable();
 
 
-let tg = window.Telegram.WebApp;
-tg.expand(); //расширяем на все окно  
+// //задаем формат для поле объем, чтобы только цифры были
+// document.getElementById('summary').addEventListener('input', function() {
+//   this.value = this.value.replace(/[^\d]/g, '').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
+// });
 
-tg.MainButton.text = "Готово"; //изменяем текст кнопки 
-tg.MainButton.setText("Кнопка для поиска"); //изменяем текст кнопки иначе
-tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
-tg.MainButton.show();
-tg.MainButton.enable();
+// //задаем формат для поле курс, чтобы только цифры были
+// document.getElementById('kurs').addEventListener('input', function() {
+//   this.value = this.value.replace(/[^0-9.]/g, '', '')
+//   // .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$0 ');
+// });
 
-var dict = {
-   type_web_msg : "from search"
-};
 
-tg.onEvent('mainButtonClicked', function(){
-   // tg.sendData(dictstring);
-   tg.sendData("НАЙТИ КУРСЫ");
-   tg.showAlert("Хорошо, ты нажал на главную кнопку.");
 
-   tg.window.close();
-   //при клике на основную кнопку отправляем данные в строковом виде
+// Задаем селекты
+var value_opt1 =  document.getElementById("select_group_1");
+var value_opt2 =  document.getElementById("select_group_2");
+var value_opt4 =  document.getElementById("select_group_4");
+var value_opt3 =  document.getElementById("select_group_3");
+var value_opt5 =  document.getElementById("select_group_5");
+var value_opt6 =  document.getElementById("select_group_6");
+var value_opt7 =  document.getElementById("select_group_7");
+
+var kurs = document.getElementById("kurs");
+var Label_Sum = document.getElementById("summary");
+
+function onChange() {
+
+    var text_val_ask = value_opt1.options[value_opt1.selectedIndex].text;
+    var text_nal_ask = value_opt2.options[value_opt2.selectedIndex].text;
+    var text_stamp_ask = value_opt3.options[value_opt3.selectedIndex].text;
+    var text_val_bid = value_opt4.options[value_opt4.selectedIndex].text;
+    var text_nal_bid = value_opt5.options[value_opt5.selectedIndex].text;
+    var text_stamp_bid = value_opt6.options[value_opt6.selectedIndex].text;
+    var text_place = value_opt7.options[value_opt7.selectedIndex].text;
+
+   //  var val_ask_num = value_opt1.value;
+
+   //  var val_bid_num = value_opt4.value;
+
+
+
+   //  let searchValue = Math.min(val_ask_num, val_bid_num);
+   //  let selectElement = document.getElementById('select_group_1');
+   //  let options = selectElement.options;
+
+   //  for (let i = 0; i < options.length; i++) {
+   //    if (options[i].value == searchValue) {
+   //    // console.log(options[i].text, document.getElementById('summary').innerHTML);
+
+   //    document.getElementById('Label_Sum').innerHTML = 'Объем, в ' + options[i].text.substring(0, 3);
+   //    window.sum_val_name = options[i].text
+   //    // + options[i].text;
+
+   //    break;
+   //    } }
+
+
+    var jsonObject = {
+        "type_web_msg" : "from search",
+        "text_val_ask" : text_val_ask,
+        "text_nal_ask" : text_nal_ask,
+        "text_stamp_ask" : text_stamp_ask,
+        "text_val_bid" : text_val_bid,
+        "text_nal_bid" : text_nal_bid,
+        "text_stamp_bid" : text_stamp_bid,
+        "text_place": text_place
+    };
+
+
+    window.jsonString = JSON.stringify(jsonObject);
+
+
+}
+
+value_opt1.onchange = onChange;
+value_opt4.onchange = onChange;
+value_opt2.onchange = onChange;
+value_opt3.onchange = onChange;
+value_opt5.onchange = onChange;
+value_opt6.onchange = onChange;
+value_opt7.onchange = onChange;
+
+
+tg2.onEvent('mainButtonClicked', function(){
+  onChange();
+
+  tg2.sendData(jsonString);
+  // tg2.sendData("От страницы ADS");
+  // console.log(text_place);
+
+  tg2.window.close();
+  //при клике на основную кнопку отправляем данные в строковом виде
 });
-
-
